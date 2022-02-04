f337cb629d4c7e06a5040e1455b104ed69fe5bf4
cantte <cantte098@gmail.com>
2022-02-04 16:08:27 -0500

    feat: login and logout (maybe not)

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,24 @@
-import { Button } from 'ui'
+import { useUser } from '@auth0/nextjs-auth0'
 
 const Web = () => {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      {user && (
+        <>
+          <p>
+            <strong>{user.name}</strong>
+          </p>
+          <p>{user.email}</p>
+        </>
+      )}
+
+      {user === undefined && <a href="/api/auth/login">Login</a>}
+      {user !== undefined && <a href="/api/auth/logout">Logout</a>}
     </div>
   )
 }
