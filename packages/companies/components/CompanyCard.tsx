e0a79c05229baeb7fec6ac11c1b4f8564d5e9a58
cantte <cantte098@gmail.com>
2022-02-05 12:08:31 -0500

    feat(companies): new company card

--- packages/companies/components/CompanyCard.tsx
+++ packages/companies/components/CompanyCard.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, Chip, Stack, Typography } from '@mui/material'
+
 import type { Company } from '../models'
 
 type Props = {
@@ -7,11 +9,18 @@ type Props = {
 export const CompanyCard = (props: Props) => {
   const { company } = props
   return (
-    <>
-      <h1>Id: {company.id}</h1>
-      <h2>Name: {company.name}</h2>
-      <p>Info: {company.info}</p>
-      <small>Nit: {company.nit}</small>
-    </>
+    <Card elevation={3} sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Stack>
+          <Typography variant="h3" sx={{ mb: 1.5 }}>
+            {company.name}
+          </Typography>
+        </Stack>
+        <Chip label={`Nit: ${company.nit}`} sx={{ mb: 1.5 }} />
+        <Typography variant="subtitle2" sx={{ display: 'block' }}>
+          {company.info}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
