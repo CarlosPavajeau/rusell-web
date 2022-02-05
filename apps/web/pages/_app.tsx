96b7ce15f5eff7dd2f480cbf67f6011bc6d477cf
cantte <cantte098@gmail.com>
2022-02-05 12:09:23 -0500

    feat(web): add @mui

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { UserProvider } from '@auth0/nextjs-auth0'
+import { Container } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <Container maxWidth="sm">
+        <Component {...pageProps} />
+      </Container>
     </UserProvider>
   )
 }
