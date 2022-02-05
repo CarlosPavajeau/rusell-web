52251aa17e982b345e4986c79b91eaa6808ef4d7
cantte <cantte098@gmail.com>
2022-02-04 20:14:22 -0500

    refactor: import core module

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
-const withTM = require("next-transpile-modules")(["ui", "companies"]);
+const withTM = require('next-transpile-modules')(['ui', 'companies', 'core'])
 
 module.exports = withTM({
   reactStrictMode: true,
-});
+})
