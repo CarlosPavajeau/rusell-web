956e611898873b17c38ca7fbc53b806e5796c801
cantte <cantte098@gmail.com>
2022-02-09 16:27:47 -0500

    feat(bank-drafts): bank draft registration form added

---
+++ packages/bank-drafts/components/BankDraftForm.tsx
@@ -0,0 +1,108 @@
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
+import { Clients } from 'clients/models'
+import { useForm } from 'react-hook-form'
+
+import { CreateBankDraftRequest } from '../models'
+
+type Props = {
+  onSubmit: (bankDraft: CreateBankDraftRequest) => void
+  clients: Clients
+  dispatcherId: string
+}
+
+const BankDraftForm = (props: Props) => {
+  const { onSubmit, clients, dispatcherId } = props
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<CreateBankDraftRequest>({
+    defaultValues: {
+      dispatcherId,
+    },
+  })
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        <FormControl>
+          <InputLabel id="sender">Sender</InputLabel>
+          <Select
+            labelId="sender"
+            label="Sender"
+            defaultValue=""
+            {...register('senderId', {
+              required: { value: true, message: 'Sender is required' },
+            })}
+          >
+            {clients
+              .filter(client => client.id !== watch('receiverId'))
+              .map(client => (
+                <MenuItem key={client.id} value={client.id}>
+                  {client.id} {client.fullName}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel id="receiver">Receiver</InputLabel>
+          <Select
+            labelId="receiver"
+            label="receiver"
+            defaultValue=""
+            {...register('receiverId', {
+              required: { value: true, message: 'Receiver is required' },
+            })}
+          >
+            {clients
+              .filter(client => client.id !== watch('senderId'))
+              .map(client => (
+                <MenuItem key={client.id} value={client.id}>
+                  {client.id}, {client.fullName}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+
+        <TextField
+          label="Amount"
+          type="number"
+          helperText={errors.amount?.message}
+          error={!!errors.amount}
+          {...register('amount', {
+            required: { value: true, message: 'Amount is required' },
+          })}
+        />
+
+        <TextField
+          label="Cost"
+          type="number"
+          helperText={errors.cost?.message}
+          error={!!errors.cost}
+          {...register('cost', {
+            required: { value: true, message: 'Cost is required' },
+          })}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ width: 'fit-content' }}
+        >
+          Register bank draft
+        </Button>
+      </Stack>
+    </form>
+  )
+}
+
+export default BankDraftForm
