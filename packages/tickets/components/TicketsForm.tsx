6c9a2acd43b888a63b4eaec814e0578a235e6224
cantte <cantte098@gmail.com>
2022-02-09 17:00:35 -0500

    feat(tickets): ticket registration form added

---
+++ packages/tickets/components/TicketsForm.tsx
@@ -0,0 +1,74 @@
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
+import { Clients } from 'clients/models'
+import { useForm } from 'react-hook-form'
+
+import { CreateTicketRequest } from '../models'
+
+type Props = {
+  clients: Clients
+  seatPrice: number
+  onSubmit: (data: CreateTicketRequest) => void
+}
+
+const TicketsForm = (props: Props) => {
+  const { clients, seatPrice, onSubmit } = props
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateTicketRequest>({
+    defaultValues: {
+      seatPrice,
+    },
+  })
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        <FormControl>
+          <InputLabel id="client">Client</InputLabel>
+          <Select
+            labelId="client"
+            label="Client"
+            defaultValue=""
+            {...register('clientId', {
+              required: { value: true, message: 'Client is required' },
+            })}
+          >
+            {clients.map(client => (
+              <MenuItem key={client.id} value={client.id}>
+                {client.id}, {client.fullName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <TextField
+          label="Seats"
+          type="number"
+          helperText={errors.seats?.message}
+          error={!!errors.seats}
+          {...register('seats', {
+            required: { value: true, message: 'Seats is required' },
+          })}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ width: 'fit-content' }}
+        >
+          Register ticket
+        </Button>
+      </Stack>
+    </form>
+  )
+}
+
+export default TicketsForm
