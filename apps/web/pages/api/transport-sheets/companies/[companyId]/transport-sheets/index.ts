62ebc6901de44c76faecda9f0e2d2d1d3717e754
cantte <cantte098@gmail.com>
2022-02-11 11:08:59 -0500

    feat(transport-sheets): api routes added

---
+++ apps/web/pages/api/transport-sheets/companies/[companyId]/transport-sheets/index.ts
@@ -0,0 +1,25 @@
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { TransportSheetsService } from 'transport-sheets/service'
+import withBearerToken from 'utils/auth0/withBearerToken'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  const { companyId } = request.query
+
+  if (request.method === 'POST') {
+    const { body } = request
+    const id = await TransportSheetsService.save(body, companyId as string)
+
+    response.status(200).json(id)
+  }
+
+  if (request.method === 'GET') {
+    const transportSheets = await TransportSheetsService.fetchAllByCompany(
+      companyId as string,
+    )
+
+    response.status(200).json(transportSheets)
+  }
+}
+
+export default withApiAuthRequired(withBearerToken(handler))
