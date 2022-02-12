b2eeb0d532f3715d5b30c3347e5c0214b20926be
cantte <cantte098@gmail.com>
2022-02-11 19:23:01 -0500

    feat: new home page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,26 +1,67 @@
-import { useUser } from '@auth0/nextjs-auth0'
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { Box, Button, Link, Typography } from '@mui/material'
+import Head from 'next/head'
+import { useContext } from 'react'
+import DashboardLayout from 'ui/layouts/DashboardLayout'
+import ThemeContext from 'ui/theme/ThemeContext'
 
 const Web = () => {
   const { user, error, isLoading } = useUser()
+  const { toggleTheme, mode } = useContext(ThemeContext)
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>{error.message}</div>
 
   return (
-    <div>
-      {user && (
-        <>
-          <p>
-            <strong>{user.name}</strong>
-          </p>
-          <p>{user.email}</p>
-        </>
-      )}
+    <>
+      <Head>
+        <title>Rusell</title>
+      </Head>
+      <Box sx={{ p: 4 }}>
+        {user && (
+          <>
+            <Typography variant="h5">
+              <strong>{user.name}</strong>
+            </Typography>
+            <Typography variant="caption">{user.email}</Typography>
+          </>
+        )}
 
-      {user === undefined && <a href="/api/auth/login">Login</a>}
-      {user !== undefined && <a href="/api/auth/logout">Logout</a>}
-    </div>
+        <Box sx={{ my: 3 }}>
+          {user === undefined && (
+            <Button variant="contained" color="primary">
+              <Link href="/api/auth/login" sx={{ textDecoration: 'none' }}>
+                Login
+              </Link>
+            </Button>
+          )}
+          {user !== undefined && (
+            <Button variant="outlined" color="error">
+              <Link
+                href="/api/auth/logout"
+                color="error"
+                sx={{ textDecoration: 'none' }}
+              >
+                Logout
+              </Link>
+            </Button>
+          )}
+        </Box>
+
+        <Box sx={{ my: 3 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => toggleTheme(mode === 'light' ? 'dark' : 'light')}
+          >
+            Toggle Theme
+          </Button>
+        </Box>
+      </Box>
+    </>
   )
 }
 
-export default Web
+Web.Layout = DashboardLayout
+
+export default withPageAuthRequired(Web)
