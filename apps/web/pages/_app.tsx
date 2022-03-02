99ac9257a155deade19196f37d451005be16e1b5
cantte <cantte098@gmail.com>
2022-03-02 12:20:32 -0500

    refactor: remove ThemeConfig

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -10,7 +10,6 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { IntlProvider } from 'react-intl'
 import DefaultLayout from 'ui/layouts/DefaultLayout'
 import { darkTheme, lightTheme } from 'ui/theme/shared'
-import ThemeConfig from 'ui/theme/ThemeConfig'
 
 type NextPageWithLayout = NextPage & {
   Layout?: NextComponentType
@@ -32,24 +31,22 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
       messages={pageProps.intlMessages}
     >
       <UserProvider>
-        <ThemeConfig>
-          <NextThemesProvider
-            defaultTheme="system"
-            attribute="class"
-            value={{
-              light: lightTheme.className,
-              dark: darkTheme.className,
-            }}
-          >
-            <NextUIProvider>
-              <CompanyProvider>
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </CompanyProvider>
-            </NextUIProvider>
-          </NextThemesProvider>
-        </ThemeConfig>
+        <NextThemesProvider
+          defaultTheme="system"
+          attribute="class"
+          value={{
+            light: lightTheme.className,
+            dark: darkTheme.className,
+          }}
+        >
+          <NextUIProvider>
+            <CompanyProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </CompanyProvider>
+          </NextUIProvider>
+        </NextThemesProvider>
       </UserProvider>
     </IntlProvider>
   )
